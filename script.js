6abf7d7fe62ae52820cb0c70ac8cd3e432fc07fa
tdbnz-coding <49893160+tdbnz-coding@users.noreply.github.com>
2024-06-02 17:17:37 +1200

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* script.js */
 function updateDateTime() {
     const now = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
